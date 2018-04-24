741de8d432cd2e14ba1563007330f8781907a9ba
邢昱恺 <33726466+Hsing-Yukai@users.noreply.github.com>
2018-04-24 12:20:23 +0800

    Update Template.h

--- Template.h
+++ Template.h
@@ -30,16 +30,14 @@ public:
 	/* 拓扑排序 */
 	vector<int> topological_sort(vector<list<int>> adjacency_list);
 	/* Dijkstra(堆优化)*/
-	void dijkstra(vector<vector<int>> adjacency_matrix, vector<bool> &known, vector<int> &distance, int source);
+	void dijkstra(vector<vector<int>> adjacency_matrix, vector<bool> &known, vector<int> &Distance, int source);
 	/* 最长无重复子串 */
 	int Longest_substring(string s);
 	/* Manacher算法(最长回文)*/
 	pair<int, int> manacher(string &s);
 	/* KMP(字符串匹配)*/
 	int KMP(string a, string b);
-	/* Bash博弈*/
-	bool Bash_game(int n, int m);
-	/* 并查集(加权优化)*/
+	/* 并查集(加权优化+路径压缩)*/
 	class union_find
 	{
 	public:
@@ -62,6 +60,7 @@ private:
 ***    !res[i]:只需判断p为素数的情况，若p非素数则p与p的倍数在之前的循环已经标记过
 ***    i * i:内层循环只需从i*i开始因为之前的循环已经标记过i * x(x<i)的情况
 */
+
 vector<bool> Template::Eratosthenes_Sieve(int n)
 {
 	vector<bool> res(n+1, false);
@@ -80,16 +79,15 @@ vector<bool> Template::Eratosthenes_Sieve(int n)
 	return res;
 }
 
-
 /*欧几里得算法（辗转相除法）
 **找出a与b的最大公约数
 **返回参数为a与b的最大公约数
 */
+
 int Template::gcd(int a, int b)
 {
 	return b == 0 ? a : gcd(b, a%b);
 }
-
 
 /*扩展欧几里得算法
 **找出ax + by = gcd(a,b)的一个x,y整数解
@@ -100,6 +98,7 @@ int Template::gcd(int a, int b)
 ***			  则可得等式a(x1) + b(y1) = a(y2) + b(x2 - (a/b)*y2)视a,b为未知数由等式恒等定理可得
 ***		   	  递推关系 x1 = y2 , y1 = x2 - (a/b)*y2;
 */
+
 void Template::ex_gcd(int a, int b, int &x, int &y)
 {
 	if (b == 0)
@@ -114,12 +113,12 @@ void Template::ex_gcd(int a, int b, int &x, int &y)
 	y = x1 - (a / b)*y1;
 }
 
-
 /*分解质因数（唯一分解定理）
 **将整数n用多个质数相乘的形式表示
 **返回参数res中的元素即为n的质数因子
 ***算术基本定理可表述为：任何一个大于1的自然数 N,如果N不为质数，那么N可以唯一分解成有限个质数的乘积;
 */
+
 vector<int> Template::Prime_Factor(int n)
 {
 	vector<int> res;
@@ -134,11 +133,11 @@ vector<int> Template::Prime_Factor(int n)
 	return res;
 }
 
-
 /*归并排序
 **以归并排序的方法排序容器target
 ***
 */
+
 void Template::merge_sort(vector<int> &target)
 {
 	vector<int> copy = target;
@@ -163,10 +162,10 @@ void Template::merge_sort_recursive(vector<int> &target, std::vector<int> &copy,
 	}
 }
 
-
 /*快速排序
 **以快速排序的方法排序容器vector
 */
+
 void Template::quick_sort(vector<int> &target)
 {
 	quick_sort_recursive(target, 0, target.size() - 1);
@@ -187,7 +186,6 @@ void Template::quick_sort_recursive(vector<int> &target, int start, int end)
 	quick_sort_recursive(target, flag + 1, end);
 }
 
-
 /*拓扑排序
 **以带队列的方式对图进行拓扑排序
 **返回参数为排序后顶点的顺序
@@ -195,6 +193,7 @@ void Template::quick_sort_recursive(vector<int> &target, int start, int end)
 ***		1：不断的弹出队列中的顶点元素，每弹出一个顶点元素，标记此顶点并将计数器加1，然后通过邻接列表访问此顶点指向的所有顶点
 ***		3：将每个顶点的入度减1，若减1后入度为0则将此顶点存入队列 返回第0步
 */
+
 vector<int> Template::topological_sort(vector<list<int>> adjacency_list)
 {
 	map<int, int> vertices_indgree;										//全部顶点的入度表 first为顶点名称 second为此顶点的入度
@@ -247,7 +246,6 @@ vector<int> Template::topological_sort(vector<list<int>> adjacency_list)
 	return res;
 }
 
-
 /*Dijkstra(堆优化)
 **参数列表中:adjacency_matrix[a][b]的值若为0则代表a不与b相连，若值大于0则为a到b的边的权重
 **		   source代表原点，以该点进行路径计算
@@ -257,44 +255,50 @@ vector<int> Template::topological_sort(vector<list<int>> adjacency_list)
 ***    2.访问所有该点所指向的点，并以最小值的方式更改distance
 ***    3.若被指向的点从未被访问过，则将其压入优先队列中
 */
-void Template::dijkstra(vector<vector<int>> adjacency_matrix, vector<bool> &known, vector<int> &distance, int source)
+
+
+void Template::dijkstra(vector<vector<int>> adjacency_matrix,vector<bool> &known,vector<int> &Distance, int source)
 {
 	/*cmp 函数 此处无法实现
-	struct cmp
-	{
-		bool operator()(int a,int b)
-		{
-			return distance[a]>distance[b];
-		}
-	};
+	struct cmp  
+    {  
+        bool operator()(int a,int b)  
+        {   
+            return Distance[a]>Distance[b];  
+        }   
+    }; 
 	*/
-	distance[source] = 0;
+	Distance[source] = 0;
 	priority_queue<int, vector<int>> que;	//此处需改写为priority_queue<int, vector<int>,cmp> que;
 	que.push(source);
 	while (!que.empty())
 	{
 		int tmp = que.top();
-		que.pop();
 		known[tmp] = true;
+		que.pop();
 		for (int i = 0; i < adjacency_matrix[tmp].size(); i++)
 		{
-			if (adjacency_matrix[tmp][i] == 0 || known[i])
+			if (adjacency_matrix[tmp][i] == 0)
 				continue;
-			if (distance[i] == -1 || distance[tmp] + adjacency_matrix[tmp][i] < distance[i])
+			if (Distance[i] == -1 || Distance[i]>Distance[tmp] + adjacency_matrix[tmp][i])
 			{
-				distance[i] = distance[tmp] + adjacency_matrix[tmp][i];
-				que.push(i);
+				Distance[i] = Distance[tmp] + adjacency_matrix[tmp][i];
+				if (!known[i])
+				{
+					known[i] = true;
+					que.push(i);
+				}
 			}
 		}
 	}
 }
-
 
 /*最长无重复子串
 **找出串s的最长无重复子串 例如"abcabcbb"的最长无重复子串为"abc"长度为3
 **返回参数即为最长无重复子串的长度
 ***
 */
+
 int Template::Longest_substring(string s)
 {
 	int length = s.length(), res = 0;
@@ -318,7 +322,6 @@ int Template::Longest_substring(string s)
 	return res;
 }
 
-
 /*Mannacher算法(最长回文)
 **返回参数中first代表最长回文长度, second代表最长回文的对称点位置
 ***解释：0：通过在字符串中插入间隔符消除回文长度奇偶性的问题(此时回文的长度必定为奇数)
@@ -332,6 +335,7 @@ int Template::Longest_substring(string s)
 ***     4：标记radius[i]后继续以i为轴进行探测，当左右两端字符不相等时终止，每次探测成功便对radius[i]++
 ***     5：探测完毕后尝试更新max_right,max_right_pos与res
 */
+
 pair<int, int> Template::manacher(string &s)
 {
 	//对字符串插入标记
@@ -368,10 +372,10 @@ pair<int, int> Template::manacher(string &s)
 	return res;
 }
 
-
 /*KMP算法(字符串匹配)
 **
 */
+
 int Template::KMP(string a, string b)
 {
 	//构建部分匹配表
@@ -402,19 +406,6 @@ int Template::KMP(string a, string b)
 	return -1;
 }
 
-
-/*Bash博弈:有一堆物品共n个，两个人轮流从这堆物品中取物，规定每次至少取一个最多取m个,最后取光者得胜。
-**返回参数若为true则先手胜，若为false的后手胜
-***解释:若n=m+1则不论先手拿走多少个，后手皆可一次拿完2
-***    若n=(m+1)x+s则先手拿走s个后，后手拿走k个(k<=m)，先手拿走m+1-k个，最终先手必胜
-***    可以看出若n%(m+1)!=0则先手必胜，反之先手必败
-*/
-bool Template::Bash_game(int n, int m)
-{
-	return n % (m + 1) != 0;
-}
-
-
 /*并查集
 **通过加权树的方法对其优化，使时间复杂度降至最低
 ***解释：1.引入树的结构用来表示连通分量，初始时有n个数(n个连通分量)
@@ -423,6 +414,7 @@ bool Template::Bash_game(int n, int m)
 ***       只有根节点的id等于根节点的名称，同时也代表着所属连通分量的id
 ***     3.每次进行find操作时若此节点非根节点，则不断迭代直至找出根节点，找出根节点后即可获取所属连通分量的id
 */
+
 Template::union_find::union_find(int n)
 {
 	id.resize(n);
@@ -435,10 +427,10 @@ Template::union_find::union_find(int n)
 }
 int Template::union_find::find(int x)
 {
-	while (x != id[x])
-	{
-		x = id[x];
-	}
+	if (x != id[x])
+    {
+        id[x] = find(id[x]);     
+    }   
 	return x;
 }
 void Template::union_find::Union(int x1, int x2)
